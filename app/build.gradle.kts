c1337192e9ef94ea79b00a9a25201c7417929b67
nsr6 <nsr6@users.noreply.github.com>
2026-09-11 12:06:25 +0530

    Align Java and Kotlin compiler targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ android {
     }
     buildFeatures { compose = true; buildConfig = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     kotlinOptions { jvmTarget = "17" }
 }
 
